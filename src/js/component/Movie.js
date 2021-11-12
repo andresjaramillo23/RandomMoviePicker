ce655cded8eae12346418d725846c8165306194d
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-11-12 16:22:33 +0000

    Fixed button with use state and Added index function to do the random selection

--- src/js/component/Movie.js
+++ src/js/component/Movie.js
@@ -1,52 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import useSWR from "swr";
 import { fetcher } from "../fetcher";
 
 import { Col, Card, Button } from "react-bootstrap";
-import "../../styles/index.scss";
 
 const Movie = () => {
-	const { data: MostPopularMovies, isValidating } = useSWR("/en/API/MostPopularMovies/k_z5d6y3ag", fetcher);
+	const [movieIndex, setMovieIndex] = useState(0);
 
-	function pickARdmMovie() {
-		return MostPopularMovies && MostPopularMovies.items[Math.floor(Math.random() * MostPopularMovies.items.length)];
-	}
+	const { data: mostPopularMovies, isValidating } = useSWR("/en/API/MostPopularMovies/k_2c1sizm2", fetcher, {
+		refreshInterval: 0,
+		revalidateOnFocus: false
+	});
 
-	function pickAnother() {
-		console.log(movie);
-		movie = pickARdmMovie();
-	}
-	let movie = pickARdmMovie();
+	let movie = React.useMemo(() => mostPopularMovies && mostPopularMovies.items[movieIndex], [
+		mostPopularMovies,
+		movieIndex
+	]);
+
+	useEffect(
+		() => {
+			if (mostPopularMovies) {
+				setMovieIndex(Math.floor(Math.random() * mostPopularMovies.items.length));
+			}
+		},
+		[mostPopularMovies]
+	);
 
 	return (
 		<Col xs={3}>
 			{isValidating &&
-				!movie && (
+				!mostPopularMovies && (
 					<div className="spinner-border" role="status">
 						<span className="visually-hidden">Loading...</span>
 					</div>
 				)}
-			{movie && (
-				<Card className="cardColors">
-					<Card.Img variant="top" src={movie.image} />
-					<Card.Body>
-						<Card.Title>
-							Title: {movie.title}
-							<br />
-							Year: {movie.year}
-						</Card.Title>
-						<Button variant="primary" className="mr-3" onClick={pickAnother()}>
-							Change Movie
-						</Button>
-						<a href={"https://www.imdb.com/title/" + movie.id} className="w-100 d-flex justify-content-end">
-							<Button variant="primary" className="mr-3">
-								Learn More
+			{mostPopularMovies &&
+				movie && (
+					<Card className="cardColors">
+						<Card.Img variant="top" src={movie.image} />
+						<Card.Body>
+							<Card.Title>
+								Title: {movie.title}
+								<br />
+								Year: {movie.year}
+							</Card.Title>
+							<Button
+								variant="primary"
+								className="mr-3"
+								onClick={() => {
+									setMovieIndex(Math.floor(Math.random() * mostPopularMovies.items.length));
+								}}>
+								Change Movie
 							</Button>
-						</a>
-					</Card.Body>
-				</Card>
-			)}
+							<a
+								href={"https://www.imdb.com/title/" + movie.id}
+								className="w-100 d-flex justify-content-end">
+								<Button variant="primary" className="mr-3">
+									Learn More
+								</Button>
+							</a>
+						</Card.Body>
+					</Card>
+				)}
 		</Col>
 	);
 };
